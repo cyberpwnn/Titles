a5fdf8cd25963f0826b6c62ddda16bd907bfe586
Daniel Mills <danielmillst@gmail.com>
2016-12-10 22:54:14 -0500

    PDC converter

--- src/main/java/org/cyberpwn/titles/PlayerDataController.java
+++ src/main/java/org/cyberpwn/titles/PlayerDataController.java
@@ -1,25 +1,21 @@
 package org.cyberpwn.titles;
 
+import java.util.List;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.phantomapi.Phantom;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.phantomapi.clust.ConfigurationHandler;
+import org.phantomapi.clust.PD;
 import org.phantomapi.construct.Controllable;
 import org.phantomapi.construct.Controller;
-import org.phantomapi.lang.GMap;
-import org.phantomapi.lang.GSet;
+import org.phantomapi.lang.GList;
+import org.phantomapi.sync.TaskLater;
 
 public class PlayerDataController extends Controller
 {
-	private GMap<Player, PlayerData> cache;
-	private GSet<Player> flush;
-	
 	public PlayerDataController(Controllable parentController)
 	{
 		super(parentController);
-		
-		flush = new GSet<Player>();
-		cache = new GMap<Player, PlayerData>();
 	}
 	
 	@Override
@@ -31,67 +27,66 @@ public class PlayerDataController extends Controller
 	@Override
 	public void onStop()
 	{
-		for(Player i : Phantom.instance().onlinePlayers())
-		{
-			save(i);
-		}
-	}
-	
-	public boolean contains(Player player)
-	{
-		return cache.contains(player);
-	}
-	
-	public PlayerData get(Player p)
-	{
-		if(!contains(p))
-		{
-			load(p);
-		}
 		
-		return cache.get(p);
-	}
-	
-	public void flush()
-	{
-		for(Player i : flush)
-		{
-			saveMysql(cache.get(i));
-		}
-		
-		flush.clear();
 	}
 	
 	public void load(Player player)
 	{
-		if(!contains(player))
+		PlayerData pd = new PlayerData(player);
+		
+		try
 		{
-			PlayerData pd = new PlayerData(player);
-			loadMysql(pd);
-			cache.put(player, pd);
+			if(ConfigurationHandler.rowExists(pd, getSQL()))
+			{
+				loadMysql(pd);
+				PD.get(player).getConfiguration().set("titles.t", pd.titles);
+				PD.get(player).getConfiguration().set("titles.c", pd.currentTitle);
+				
+				new TaskLater()
+				{
+					@Override
+					public void run()
+					{
+						try
+						{
+							ConfigurationHandler.dropRow(pd, getSQL());
+						}
+						
+						catch(Exception e)
+						{
+							
+						}
+					}
+				};
+			}
+			
+			List<String> titles = PD.get(player).getConfiguration().getStringList("titles.t");
+			String title = PD.get(player).getConfiguration().getString("titles.c");
+			
+			if(titles == null)
+			{
+				titles = new GList<String>();
+			}
+			
+			if(title == null)
+			{
+				title = "";
+			}
+			
+			PD.get(player).getConfiguration().set("titles.t", titles);
+			PD.get(player).getConfiguration().set("titles.c", title);
+			PD.get(player).flush();
 		}
-	}
-	
-	public void flush(Player player)
-	{
-		if(contains(player))
+		
+		catch(Exception e)
 		{
-			flush.add(player);
-		}
-	}
-	
-	public void save(Player player)
-	{
-		if(contains(player))
-		{
-			saveMysql(cache.get(player));
-			cache.remove(player);
+			
 		}
 	}
 	
 	@EventHandler
-	public void on(PlayerQuitEvent e)
+	public void on(PlayerJoinEvent e)
 	{
-		save(e.getPlayer());
+		load(e.getPlayer());
 	}
 }
